de6fc1ebe661d4347c266db826a2a7b1dbea6a06
Dursun İnel <drsinl1905@gmail.com>
2024-05-08 19:51:39 +0300

    .

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -7,6 +7,7 @@ function Header({ coin }) {
         <div className="header_flex">
           <div>
             <h1>TyconnTech</h1>
+            
           </div>
           <div className="current_price">
             Güncel Para : <span>{formatCoin(coin)}</span>
